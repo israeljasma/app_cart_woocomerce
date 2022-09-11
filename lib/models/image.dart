0e8c865124189a1af7e397202f591c2549ba7261
Israel Jasma <israel.jasma@gmail.com>
2022-09-11 03:13:55 -0300

    Feat: Image model

---
+++ lib/models/image.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+class Image {
+  Image({
+    required this.id,
+    required this.dateCreated,
+    required this.dateCreatedGmt,
+    required this.dateModified,
+    required this.dateModifiedGmt,
+    required this.src,
+    required this.name,
+    required this.alt,
+  });
+
+  int id;
+  DateTime dateCreated;
+  DateTime dateCreatedGmt;
+  DateTime dateModified;
+  DateTime dateModifiedGmt;
+  String src;
+  String name;
+  String alt;
+
+  factory Image.fromJson(String str) => Image.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Image.fromMap(Map<String, dynamic> json) => Image(
+        id: json["id"],
+        dateCreated: DateTime.parse(json["date_created"]),
+        dateCreatedGmt: DateTime.parse(json["date_created_gmt"]),
+        dateModified: DateTime.parse(json["date_modified"]),
+        dateModifiedGmt: DateTime.parse(json["date_modified_gmt"]),
+        src: json["src"],
+        name: json["name"],
+        alt: json["alt"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "date_created": dateCreated.toIso8601String(),
+        "date_created_gmt": dateCreatedGmt.toIso8601String(),
+        "date_modified": dateModified.toIso8601String(),
+        "date_modified_gmt": dateModifiedGmt.toIso8601String(),
+        "src": src,
+        "name": name,
+        "alt": alt,
+      };
+}
