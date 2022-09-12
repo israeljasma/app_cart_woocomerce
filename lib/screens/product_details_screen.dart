f2fa36b74c88c78ac60a837cc279146e1d306529
Israel Jasma <israel.jasma@gmail.com>
2022-09-11 22:47:33 -0300

    Feat: Product Detail Screen

---
+++ lib/screens/product_details_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatefulWidget {
+  const ProductDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductDetailScreen> createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('ProductDetailScreen'),
+      ),
+    );
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,5 +2,7 @@ export 'package:app_cart_woocomerce/screens/account_screen.dart';
 export 'package:app_cart_woocomerce/screens/bag_screen.dart';
 export 'package:app_cart_woocomerce/screens/favourite_screen.dart';
 export 'package:app_cart_woocomerce/screens/home_screen.dart';
+export 'package:app_cart_woocomerce/screens/login_screen.dart';
+export 'package:app_cart_woocomerce/screens/product_details_screen.dart';
 export 'package:app_cart_woocomerce/screens/product_screen.dart';
 export 'package:app_cart_woocomerce/screens/store_screen.dart';
