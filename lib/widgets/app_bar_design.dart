43c789c091a55b13d8c629c5c0e487820041bbf6
Israel Jasma <israel.jasma@gmail.com>
2022-09-26 22:26:39 -0300

    Feat: Badge in cart icon

--- lib/widgets/app_bar_design.dart
+++ lib/widgets/app_bar_design.dart
@@ -1,4 +1,7 @@
+import 'package:app_cart_woocomerce/providers/cart_provider.dart';
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AppBarDesign extends StatelessWidget implements PreferredSizeWidget {
   const AppBarDesign({Key? key}) : super(key: key);
@@ -6,14 +9,32 @@ class AppBarDesign extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
   @override
   Widget build(BuildContext context) {
+    final cartProvider = Provider.of<CartProvider>(context);
     return AppBar(
       title: const Text('App Cart Woocomerce'),
       actions: <Widget>[
-        IconButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/cart', arguments: 'movie-instance');
-          },
-          icon: const Icon(Icons.shopping_cart),
+        Badge(
+          showBadge: cartProvider.cart.isEmpty ? false : true,
+          badgeContent: Text(
+            cartProvider.cart.length.toString(),
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 10,
+            ),
+          ),
+          position: BadgePosition.topEnd(end: 3, top: 3),
+          animationType: BadgeAnimationType.scale,
+          child: IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/cart',
+                  arguments: 'movie-instance');
+            },
+            icon: const Icon(
+              Icons.shopping_cart,
+              size: 25,
+            ),
+          ),
         ),
       ],
     );
