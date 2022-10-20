90ca7c39a0b6218e3e1fcddd7bb89e647a90fd76
Israel Jasma <israel.jasma@gmail.com>
2022-10-20 03:28:32 -0300

    Feat: Login account

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_cart_woocomerce/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -5,6 +6,10 @@ class AccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isLogin = true;
+    if (isLogin) {
+      return LoginScreen();
+    }
     return const Scaffold(
       body: Center(
         child: Text('AccountScreen'),
