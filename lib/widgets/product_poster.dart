b0828d2c9f268e8d69443f062a492ecfaf5e0de7
Israel Jasma <israel.jasma@gmail.com>
2022-10-23 01:40:20 -0300

    Feat: Cambios de diseño en productPoster

--- lib/widgets/product_poster.dart
+++ lib/widgets/product_poster.dart
@@ -1,6 +1,7 @@
 import 'package:app_cart_woocomerce/models/models.dart';
 import 'package:app_cart_woocomerce/providers/providers.dart';
 import 'package:app_cart_woocomerce/screens/screens.dart';
+import 'package:app_cart_woocomerce/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -33,15 +34,32 @@ class ProductPoster extends StatelessWidget {
         },
         child: Column(
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: FadeInImage(
-                placeholder: const AssetImage('assets/loading-image.png'),
-                image: NetworkImage(product.images.first.src),
-                width: 150,
-                height: 190,
-                fit: BoxFit.cover,
-              ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: FadeInImage(
+                    placeholder: const AssetImage('assets/loading-image.png'),
+                    image: NetworkImage(product.images.first.src),
+                    width: 150,
+                    height: 190,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                if (product.calculateDiscount() > 0) ...[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
+                      'assets/oferta-image.png',
+                      alignment: AlignmentDirectional.center,
+                      width: 150,
+                      height: 190,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ]
+              ],
             ),
             const SizedBox(height: 5),
             Flexible(
@@ -50,19 +68,93 @@ class ProductPoster extends StatelessWidget {
                 overflow: TextOverflow.ellipsis,
                 maxLines: 2,
                 textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
             const SizedBox(height: 5),
             Flexible(
-              child: Text(
-                product.price,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                textAlign: TextAlign.center,
-              ),
+              child: _PriceProduct(product: product),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _PriceProduct extends StatelessWidget {
+  const _PriceProduct({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final ProductModel product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (product.calculateDiscount() > 0) ...[
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(''),
+                Text(
+                  '\$${NumberFormat().thousandFormat(int.parse(product.regularPrice))}',
+                  style: const TextStyle(
+                    fontSize: 12,
+                    decorationColor: Colors.red,
+                    decoration: TextDecoration.lineThrough,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(''),
+                Text(
+                  '\$${NumberFormat().thousandFormat(int.parse(product.price))}',
+                  style: const TextStyle(
+                    // fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+          ] else ...[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(''),
+                Text(
+                  '\$${NumberFormat().thousandFormat(int.parse(product.price))}',
+                  style: const TextStyle(
+                    // fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ],
       ),
     );
   }
